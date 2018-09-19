b904cc07d782a4b8963e9d30b4cbaedbaf479ff3
Muhammad Jawad <jawad4781@gmail.com>
2018-09-19 19:00:43 +0500

    Add dynamically (EditText)

--- app/src/main/java/pk/edu/scocs/listview/MainActivity.java
+++ app/src/main/java/pk/edu/scocs/listview/MainActivity.java
@@ -5,44 +5,39 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-boolean backPressed = false;
+    boolean backPressed = false;
+
+    EditText etData;
+    ListView lvList;
+    Button btAdd;
+
+    ArrayList<String> arrayList;
+    ArrayAdapter<String> arrayAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ListView lvCars = findViewById(R.id.lvCars);
-        String[] cars = {
-                "Bugatti",
-                "Honda Civic",
-                "Honda City",
-                "Lamborghini",
-                "Porsche",
-                "Alto",
-                "Corolla",
-                "Vitz",
-                "Cultus",
-                "Ferrari",
-                "McLaren",
-                "Aston Martin",
-                "Audi",
-                "BMW",
-                "Cadillac",
-                "Chevrolet",
-                "Datsun",
-                "Ford",
-                "Mercedes"
-        };
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1, cars);
-        lvCars.setAdapter(adapter);
+        etData = findViewById(R.id.inputField);
+        lvList = findViewById(R.id.lvCars);
+        btAdd = findViewById(R.id.btnAdd);
 
+        arrayList = new ArrayList<String>();
+        arrayAdapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1, arrayList);
+        lvList.setAdapter(arrayAdapter);
+
+        addItem();
         ToastClick();
     }
 
@@ -53,18 +48,32 @@ boolean backPressed = false;
             public void onItemClick(AdapterView<?> parent, View viewClicked, int position, long id) {
                 TextView tvCarName = (TextView) viewClicked;
                 String message = "You Clicked #" + position + ". Which is " + tvCarName.getText().toString();
-                Toast.makeText(MainActivity.this,message,Toast.LENGTH_LONG).show();
+                Toast.makeText(MainActivity.this, message, Toast.LENGTH_LONG).show();
             }
         });
     }
 
     @Override
     public void onBackPressed() {
-        if(!backPressed) {
+        if (!backPressed) {
             backPressed = true;
             Toast.makeText(MainActivity.this, "Press Back Again to Exit", Toast.LENGTH_SHORT).show();
-        }
-        else
+        } else
             super.onBackPressed();
+    }
+    public void addItem() {
+        btAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String item = etData.getText().toString();
+                if(!item.trim().isEmpty()) {
+                    etData.getText().clear();
+                    arrayList.add(item);
+                    arrayAdapter.notifyDataSetChanged();
+                }
+                else
+                    Toast.makeText(MainActivity.this, "No data found. Please write something", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
